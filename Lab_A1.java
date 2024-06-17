71c573099162d51a37ba525d15069807e733720c
Devang <147418262+Devanghadiyal11@users.noreply.github.com>
2024-06-17 16:37:43 +0530

    Add files via upload

---
+++ Lab_A1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Lab_A1{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a radius : ");
+		int n = sc.nextInt();
+		double area = Math.PI*(n*n); 
+		System.out.println("Area of circle  = "+area);
+
+	}
+	
+}
