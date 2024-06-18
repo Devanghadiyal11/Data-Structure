1447650f84a4a3b1ad3067f7b54fccae43808642
Devang <147418262+Devanghadiyal11@users.noreply.github.com>
2024-06-18 16:33:37 +0530

    Add files via upload

---
+++ Lab_A4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Lab_A4{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.print("Enter number :");
+		int n =sc.nextInt();
+		int r=1;
+		int temp=1;
+		for(int i=1;i<=n;i++){
+			 r=r*i;
+			 temp =r; 
+		}
+		System.out.println("factorial of is :"+ temp);
+	}
+}
