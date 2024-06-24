8ff458b8aadc9909591201551dcfaa898ec3c3ac
Devang <147418262+Devanghadiyal11@users.noreply.github.com>
2024-06-24 20:11:57 +0530

    Update and rename Lab_9.java to Lab_2_9.java

--- Lab_2_9.java
+++ Lab_2_9.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class Lab_9{
+public class Lab_2_9{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter seconds : ");
