f9326bcd0fd22fa5c4f4eb4055f490015a936eb7
Devang <147418262+Devanghadiyal11@users.noreply.github.com>
2024-06-24 19:59:46 +0530

    Update Lab_1.java

--- Lab_1.java
+++ Lab_1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Lab_A1{
+public class Lab_1{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a radius : ");
